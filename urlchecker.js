908f36b22d498035ea7df5ec2068610f67fc7a61
D.Kerner <denkern@gmail.com>
2017-06-24 17:03:05 +0700

    changed url library for node v6.0. Improved html with description

--- urlchecker.js
+++ urlchecker.js
@@ -1,28 +1,28 @@
-const { URL } = require('url');
+const url = require('url');
 const dns = require('dns');
+
 
 module.exports = function(rawURL, callback) {
 
-	try {
-		const myURL = new URL(rawURL);
-		dns.lookup(myURL.host, (err, dnsres) => {
-			if (err) {
-				callback({"error": "unknown host: " + rawURL});
-			}
-			else {
-				callback(null, myURL.toString());
-			}
-		});
-	}
-	catch (e)
-	{
-		if (typeof(e) === TypeError)
-		{
-			callback({"error": "invalid URL: " + rawURL});
-		}
-		else
-		{
-			callback({"erorr": e.toString()});
-		}
-	}
+var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+var regex = new RegExp(expression);
+
+		if (!rawURL.match(regex))
+    {
+      callback({"error": "invalid URL: " + rawURL});  
+    }
+    else
+    {
+      let host = url.parse(rawURL).hostname;
+      
+        dns.lookup(host, (err, dnsres) => {
+          if (err) {
+            callback({"error": "unknown host: " + rawURL});
+          }
+          else {
+            callback(null, rawURL);
+          }
+        });
+    }
+	
 };
